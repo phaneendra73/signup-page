391f3f4523bc67f9c1869b60ab069cfb08ca470b
phaneendra73 <phaneendra3377@gmail.com>
2023-05-17 19:36:52 +0530

    simple login page

---
+++ script.js
@@ -0,0 +1,23 @@
+var errormessage="";
+    var missing="";
+    function isEmail(email) {
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+    }
+    $("#submitbutton").click(function () {
+    if(isEmail($("#Email").val())== false || $("#Email").val()=="" ){
+        errormessage+="<p>❌ Email id is not valid</p>"
+    }
+    if($.isNumeric($("#phoneno").val())== false || $("#Email").val()==""){
+        errormessage+="<p>❌ Phone number is invalid</p>"
+    }
+    if($("#password").val()==$("#confirm-password").val()){
+        errormessage+="<p>❌ Password doesnt match</p>"
+    }
+    if(errormessage==""){
+        $("#Result").html("✅ you have sucessfully registered");
+    }
+    else{
+        $("#Result").html(errormessage);
+    }
+    });
